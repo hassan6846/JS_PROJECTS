d550ee7a93e7001324331e64a62e26835a7f260d
Jager <96440779+hassan6846@users.noreply.github.com>
2023-01-08 12:25:22 +0500

    blunder

--- -3 GippySearchApi/script.js
+++ -3 GippySearchApi/script.js
@@ -4,9 +4,14 @@ const main=document.querySelector(".main");
 // api request in XMLHttpRequest
 
 btn.addEventListener("click", function(){
-    alert(inp.value)
+
+
+    const val=inp.value;
+//   exporting outside the function
 })
-const url=`https://api.giphy.com/v1/gifs/search?api_key=4NoUyrwgv51B1BcgzQjQ4kVOQ71cWkFg&q=${}&limit=25&offset=0&rating=g&lang=en`
+
+const val=inp.value;
+const url=`https://api.giphy.com/v1/gifs/search?api_key=4NoUyrwgv51B1BcgzQjQ4kVOQ71cWkFg&q=${val}&limit=25&offset=0&rating=g&lang=en`
 const call=new XMLHttpRequest();
 call.open("GET", url);
 call.send();
