cf47dfd0f8a13f4c6c12e17b4bd2fb537cefa2a4
Jager <96440779+hassan6846@users.noreply.github.com>
2023-01-14 20:05:05 +0500

    loading_setTimeOuts

--- -8 loading Screen/index.js
+++ -8 loading Screen/index.js
@@ -1,5 +1,11 @@
-window.addEventListener("load", function() {
-    var loadingScreen = document.getElementById("loading-screen");
+$("body").css("overflow", "hidden");
+window.addEventListener("load", function () {
+  var loadingScreen = document.getElementById("loading-screen");
+
+  setTimeout(() => {
     loadingScreen.style.display = "none";
-  });
-  
+
+    const body = document.querySelector("body");
+    $(body).css("overflow", "auto");
+  }, 3000);
+});
